ec523e0effe09d1be76244fd1ef641a35050d0ef
Hana Widyatari <hanawidyatari23@gmail.com>
2022-08-25 18:23:43 +0700

    add head in app

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,17 @@
+import Head from "next/head";
+
 import "../styles/globals.css";
 import { wrapper } from "../store/index";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>MIG-Pretest | Customer Management</title>
+      </Head>
+      <Component {...pageProps} />;
+    </>
+  );
 }
 
 export default wrapper.withRedux(MyApp);
