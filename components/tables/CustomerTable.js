6ce6145b82beb3ff9b2b850145ec0c6a91ce4aff
Hana Widyatari <hanawidyatari23@gmail.com>
2022-08-25 07:30:53 +0700

    update fetch api get data

--- components/tables/CustomerTable.js
+++ components/tables/CustomerTable.js
@@ -1,12 +1,16 @@
 import { useEffect, useState } from "react";
+import axios from "axios";
 import DataTable from "react-data-table-component";
+
+import { hasToken } from "../../utility/localStorage";
+import { Url } from "../../utility/urlApi";
 
 import SearchFilter from "../filters/SearchFilter";
 import SpinnerLoading from "../loadings/SpinnerLoading";
-import { customer } from "../../utility/customer";
 import { customerHead } from "../../common/CustomerTableHead";
 
 export default function CustomerTable() {
+  const [customers, setCustomers] = useState([]);
   const [pending, setPending] = useState(true);
   const [query, setQuery] = useState("");
 
@@ -15,6 +19,17 @@ export default function CustomerTable() {
       setPending(false);
     }, 2000);
     return () => clearTimeout(timeout);
+  }, []);
+
+  useEffect(() => {
+    hasToken();
+    axios
+      .get(`${Url}customers`)
+      .then((response) => {
+        console.log(response.data);
+        setCustomers(response.data);
+      })
+      .catch((error) => error);
   }, []);
 
   const handleData = (rowData) => {
@@ -29,7 +44,7 @@ export default function CustomerTable() {
     <>
       <SearchFilter filterValue={query} filterChange={(e) => setQuery(e.target.value)} />
       <div className="block w-full shadow rounded-lg overflow-x-auto whitespace-nowrap -ml-4 py-3  mt-6 text-sm">
-        <DataTable columns={customerHead} data={handleData(customer)} pagination progressPending={pending} progressComponent={<SpinnerLoading />} highlightOnHovers defaultSortField="name" />
+        <DataTable columns={customerHead} data={handleData(customers.data)} pagination progressPending={pending} progressComponent={<SpinnerLoading />} highlightOnHovers defaultSortField="name" />
       </div>
     </>
   );
